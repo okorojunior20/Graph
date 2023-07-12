0826a71550ea3fdfe863a64a4c61eda522d62864
Okoro Nwachukwu Junior <130679143+okorojunior20@users.noreply.github.com>
2023-07-12 12:19:41 +0100

    Add files via upload
    Graph that was done using PHP Loop.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php require("action.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>graph</title>
+    <!-- <link rel="stylesheet" href="./graph.css"> -->
+</head>
+<body>
+
+     <div class="mainbar" style="display: block;justify-content: space-around; border: 1px solid blue;width: 60%;height: 50%;padding-bottom:20px;">
+        <div class="bar"  style="width: 450px; border: 1px solid blue;  background-color: blue; height:70px ;margin-top: 10px;margin-left:20px"></div>
+        <div class="bar" style="width: 300px;border: 1px solid blue; background-color: blue;height:70px ;margin-top: 10px;margin-left:20px"></div>
+        <div class="bar" style="width: 550px;border: 1px solid blue; background-color: blue;height:70px ;margin-top: 10px;margin-left:20px"></div>
+        <div class="bar" style="width: 650px;border: 1px solid blue; background-color: blue;height:70px ;margin-top: 10px;margin-left:20px"></div>
+        <div class="bar" style="width: 200px;border: 1px solid blue; background-color: blue;height:70px ;margin-top: 10px;margin-left:20px"></div>
+     </div>
+     
+</body>
+</html>
